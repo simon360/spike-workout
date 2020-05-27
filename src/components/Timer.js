5b59c20a04ef83279060a228ca8c0d0d9a156f23
Simon Andrews <simon@hubblehq.com>
2020-05-27 16:14:36 +0100

    Add pause, resume, and skip buttons

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,11 @@
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const formatTime = (seconds) => `:${String(seconds).padStart(2, "0")}`;
 
-export default function Timer({ length, onDone }) {
+export default function Timer({ length, key, onDone }) {
+  const [completesAt, setCompletesAt] = useState(
+    new Date().getTime() + length * 1000
+  );
   const [timeLeft, setTimeLeft] = useState(length * 1000);
   const timeLeftSeconds = useMemo(() => Math.floor(timeLeft / 1000), [
     timeLeft,
@@ -11,12 +14,24 @@ export default function Timer({ length, onDone }) {
     timeLeftSeconds,
   ]);
 
+  const [state, setState] = useState("running");
+
   useEffect(() => {
-    const started = new Date().getTime();
-    const completed = started + length * 1000;
+    // When key - ie. the set name - changes, reset the timer.
+    setTimeLeft(length * 1000);
+  }, [length, key]);
+
+  useEffect(() => {
+    if (state === "running") {
+      setCompletesAt(new Date().getTime() + timeLeft);
+    }
+  }, [timeLeft, state]);
+
+  useEffect(() => {
+    if (state === "paused") return;
 
     const interval = setInterval(() => {
-      const newTimeLeft = completed - new Date().getTime();
+      const newTimeLeft = completesAt - new Date().getTime();
 
       if (newTimeLeft < 0) {
         setTimeLeft(0);
@@ -28,7 +43,20 @@ export default function Timer({ length, onDone }) {
     }, 100);
 
     return () => clearInterval(interval);
-  }, [length, onDone]);
+  }, [length, onDone, completesAt, state]);
 
-  return timeLeftDisplay;
+  return (
+    <>
+      <h3>{timeLeftDisplay}</h3>
+      <div>
+        {state === "running" && (
+          <button onClick={() => setState("paused")}>Pause</button>
+        )}
+        {state === "paused" && (
+          <button onClick={() => setState("running")}>Resume</button>
+        )}
+        <button onClick={() => onDone()}>Skip to next</button>
+      </div>
+    </>
+  );
 }
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -7,10 +7,8 @@ export default function Workout({ name, instructions, length, onDone }) {
   return (
     <div className="Workout">
       <h2>{name}</h2>
-      <p>{instructions}</p>
-      <h3>
-        <Timer length={length} onDone={onDone} />
-      </h3>
+      {instructions && <p>{instructions}</p>}
+      <Timer length={length} key={name} onDone={onDone} />
     </div>
   );
 }
